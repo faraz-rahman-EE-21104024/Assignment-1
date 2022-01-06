6e6fa0f33a8d413a08d7e12979cd828142660bfa
faraz-rahman-EE-21104024 <97098058+faraz-rahman-EE-21104024@users.noreply.github.com>
2022-01-06 21:07:26 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,27 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:\Users\Faraz Rahman\Desktop\Assignment 1.py ============
+Enter the first number: 50
+Enter the second number: 25
+Enter the third number: 100
+The average of the three numbers is: 58.333333333333336
+Enter your gross income: 50000
+Enter number of dependents: 2
+Your income tax is: 6800.0
+Your net income is: 27200.0
+Enter your SID: 21104024
+Enter your name: Faraz Rahman
+Enter your gender (M/F/U): M
+Enter your course name: EE
+Enter your CGPA: 10
+[21104024, 'Faraz Rahman', 'M', 'EE', 10.0]
+Enter the marks of first student: 39
+Enter the marks of second student: 40
+Enter marks of third student: 45
+Enter marks of fourth student: 50
+Enter marks of fifth student: 24
+[24, 39, 40, 45, 50]
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'purple', 'Yellow']
